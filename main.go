1f9fc06d026cd24e33133ea0cb10ff9b7fa51d0a
Andrey Ivanov <norbyt@mail.ru>
2019-04-08 18:51:11 +0200

    first commit

--- main.go
+++ main.go
@@ -2,29 +2,23 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
 type User struct {
-	Id          string
 	Firstname   string
 	Lastname    string
 	Phonenumber string
 }
 
-var Users = make([]User, 0)
+var Users = make(map[string]User)
+var autoincrement int
 
 func main() {
-	///u := User{Id: "1", Firstname: "And", Lastname: "Kuku", Phonenumber: "111"}
-	///Users = append(Users, u)
-
-	Users = []User{
-		{Id: "1", Firstname: "And", Lastname: "Kuku", Phonenumber: "111"},
-		{Id: "vasya", Firstname: "And", Lastname: "Kuku", Phonenumber: "111"},
-	}
+	autoincrement = 0
+	Users["0"] = User{Firstname: "And", Lastname: "Kuku", Phonenumber: "111"}
 
 	router := httprouter.New()
 	router.GET("/user/:id", getUser)
@@ -35,27 +29,17 @@ func main() {
 
 }
 
-func findUser(users []User, id string) (User, error) {
-	for _, v := range users {
-		if v.Id == id {
-			return v, nil
-		}
-	}
-	return User{}, errors.New("данного пользователя не существует")
-}
-
 func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	id := ps.ByName("id")
 
-	user, err := findUser(Users, id)
-	if err != nil {
-		w.Write([]byte(err.Error()))
-		log.Println(err.Error())
+	user, ok := Users[id]
+	if !ok {
+		log.Println("Пользователь не найден!")
 		return
 	}
 
-	err = json.NewEncoder(w).Encode(user)
+	err := json.NewEncoder(w).Encode(user)
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
@@ -69,16 +53,22 @@ func addUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		log.Println(err)
 		return
 	}
-	Users = append(Users, user)
+
+	key := string(autoincrement)
+	Users[key] = user
+
+	autoincrement++
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
 	id := ps.ByName("id")
-	user, err := findUser(Users, id)
-	if err != nil {
-		w.Write([]byte(err.Error()))
-		log.Println(err.Error())
+	_, ok := Users[id]
+	if !ok {
+		log.Println("Пользователь не найден!")
 		return
 	}
-	log.Println(user)
+
+	delete(Users, id)
+
 }
