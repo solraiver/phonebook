dee21c02cbcdedc7488936b1ac82f5715bda23f0
Sol <norbyt@mail.ru>
2019-06-23 16:20:13 +0200

    second commit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
@@ -22,8 +23,11 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/user/:id", getUser)
+	router.GET("/user", getUsers)
 	router.POST("/user", addUser)
 	router.DELETE("/user/:id", deleteUser)
+	router.PUT("/user/:id", updateUser)
+
 	log.Println("Я запускаюсь")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
@@ -44,21 +48,49 @@ func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
 	}
+	log.Println("Пользователь", user.Firstname)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	user := Users
+	if len(user) == 0 {
+		log.Println("Пользователи не найдены!")
+		return
+	}
+	err := json.NewEncoder(w).Encode(user)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		log.Println(err.Error())
+	}
+	log.Println("Список пользователей!")
 }
 
 func addUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	user := User{}
 	err := json.NewDecoder(r.Body).Decode(&user)
-	log.Println(err, "ТЕЛО")
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	key := strconv.Itoa(autoincrement)
 
-	key := string(autoincrement)
-	Users[key] = user
-
+	if user.Firstname != "" {
+		Users[key] = user
+		log.Println("Пользователь", Users[key].Firstname, "добавлен!")
+	}
 	autoincrement++
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
+	user := User{}
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	Users[id] = user
+	log.Println("Пользователь обновлен!")
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
